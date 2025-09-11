b1b9ad4eb8666a26120cc5afd2aaa69fdcb3bfd4
Taha Mahha <142950542+Aress07@users.noreply.github.com>
2025-09-11 08:54:16 +0100

    Added solution to GCDS problem

---
+++ 1071-GreatestCommonDivisorOfStrings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        l1, l2 = len(str1), len(str2)
+
+        def isDivisor(l):
+            if l1 % l or l2 % l:
+                return False
+            
+            f1, f2 = l1 // l, l2 // l
+
+            if (str1 == f1 * str1[:l] and str2 == f2 * str1[:l]):
+                return True
+
+
+
+        for l in range(min(l1, l2), 0, -1):
+            if isDivisor(l):
+                return str1[:l]
+        return ""
